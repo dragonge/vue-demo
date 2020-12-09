827cc08db0b49a24a7f0fd0049885d4bee522f66
蔡志豪 <zhihao.cai@hand-china.com>
2020-12-09 16:04:23 +0800

    add: vue第一个页面样例

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import First from '@/components/views/First'
+import Second from '@/components/views/Second'
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +13,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/first',
+      name: 'First',
+      component: First
+    },
+    {
+      path: '/second',
+      name: 'Second',
+      component: Second
     }
   ]
 })
